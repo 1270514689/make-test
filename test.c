1eb1da7643c2cc6fb2f0dc204aa10b836f26e1cb
1270514689 <1270514689@qq.com>
2014-10-24 05:02:21 -0400

    the time 1024 09:02

--- test.c
+++ test.c
@@ -5,4 +5,9 @@ cout<<"test git function"<<endl;
 cout<<"three to modify the files"<<endl;
 cout<<"second to modify the files"<<endl;
 cout<<"this is from the 20141024 0808"<<endl;
+t
+r
+p
+q
+cout<<"hello "
 }
