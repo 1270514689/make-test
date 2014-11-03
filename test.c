c7ee330837350011fe307eb3d58d3faa7959af67
1270514689 <1270514689@qq.com>
2014-11-03 03:08:45 -0500

    20141103

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ cout<<"test git function"<<endl;
 cout<<"three to modify the files"<<endl;
 cout<<"second to modify the files"<<endl;
 cout<<"this is from the 20141024 0808"<<endl;
-cout<<"hello "
+cout<<"hello ";
 cout<<"10281045"<<endl;
 
 }
