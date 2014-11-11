a188d9ed401fe095c72d9607e04755e476050c2a
1270514689 <1270514689@qq.com>
2014-11-11 03:19:38 -0500

    20141111

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
-#include<iostream>
+#nclude<iostream>
 using namespace std;
-int main() {
-cout<<"test git function"<<endl;
-cout<<"three to modify the files"<<endl;
-cout<<"second to modify the files"<<endl;
-cout<<"this is from the 20141024 0808"<<endl;
-cout<<"hello ";
-cout<<"10281045"<<endl;
+	cout<<"three to modify the files"<<endl;
+	cout<<"second to modify the files"<<endl;
+	cout<<"this is from the 20141024 0808"<<endl;
+	cout<<"hello "10281045"<<endl;
+	cout<<"exe int the two target ont the time"<<endl; 
+}
+
+void hello() {
+	cout<<"test the file";
+	cout<<"this std gg"
+        cout<<endl;
 }
