881e902eccab890433f0867c35397e6b86d699ec
1270514689 <1270514689@qq.com>
2014-11-13 04:11:42 -0500

    20141113

--- test.c
+++ test.c
@@ -1,14 +1,13 @@
 #nclude<iostream>
 using namespace std;
-	cout<<"three to modify the files"<<endl;
-	cout<<"second to modify the files"<<endl;
-	cout<<"this is from the 20141024 0808"<<endl;
-	cout<<"hello "10281045"<<endl;
-	cout<<"exe int the two target ont the time"<<endl; 
-}
+
 
 void hello() {
 	cout<<"test the file";
 	cout<<"this std gg"
-        cout<<endl;
-}
+cout<<"three to modify the files"<<endl;
+cout<<"second to modify the files"<<endl;
+cout<<"this is from the 20141024 0808"<<endl;
+cout<<"hello "10281045"<<endl;
+cout<<"exe int the two target ont the time"<<endl; 
+}:wqout}
